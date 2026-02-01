bed13c99d8b0bdd3c167ad78f2cc75b54e508121
steve-ongera <gadafisteve001@gmail.com>
2026-02-01 16:09:25 +0300

    made updates

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,13 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+// Pinia stores
+import { createPinia } from 'pinia'
+
+const app = createApp(App)
+
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
